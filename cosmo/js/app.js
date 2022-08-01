4122ee75639c0265802c547e73a00c08d75a0667
KozhurinArtem <kozhurinartem2898@gmail.com>
2022-08-01 17:00:10 +0300

    work

--- cosmo/js/app.js
+++ cosmo/js/app.js
@@ -1,20 +1,51 @@
 Vue.createApp({
     data: () => ({
         hidden: true,
-        transition: false,
-        open: true,
+    
+        FirstName: '',
+        LastName: '',
+        openHidden: false,
+        exitHidden: true,
+        accountV: false,
+        userName: true,
+        table: true,
 
 
     }),
     methods: {
 
+
+
         start(){
-        this.hidden === true ? this.hidden = false: this.hidden = true
-        }
+            this.hidden = false
+            this.openHidden = true
+            this.exitHidden = false
+
+
+        },
+
+        exit(){
+            this.hidden = true 
+            this.exitHidden = true
+            this.openHidden = false
+            this.accountV = false
+            this.userName = true
+            this.table = true
+
+        },
+        toggleAcc(){
+            this.accountV === true ?  this.accountV = false : this.accountV = true
+        },
+
+        login(){
+            if (this.accountV === true &&  this.openHidden === false) {
+                this.accountV = true} else { this.accountV = false}
+                this.userName = false
+                this.table = false
+            
+        },
 
 
 
     }
-
-
 }).mount('#app')
